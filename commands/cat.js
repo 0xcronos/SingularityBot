9ba48b7becb1641c40be367499f7465ffead0725
0xCronos <56097041+0xCronos@users.noreply.github.com>
2020-09-16 17:40:00 -0300

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 module.exports = {
-	name: 'cat',
-	description: 'Muestra un lindo gatito aleatorio',
+    name: 'cat',
+    description: 'Muestra un lindo gatito aleatorio',
     aliases: ['cat'],
     args: false,
     guildOnly: false,
     cooldown: 5,
-	async execute(message, args) {
+    async execute(message, args) {
         const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-	    message.channel.send(file);
+	message.channel.send(file);
     }
 };
