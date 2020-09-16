c643404dc2b817f8afbcee8fe90a2a0c6e6dff7e
0xCronos <56097041+0xCronos@users.noreply.github.com>
2020-09-16 17:39:35 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,14 +6,14 @@ module.exports = {
     guildOnly: false,
     usage: "<user-mention>",
     execute(message) {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
-		}
-
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
-		});
-		
-		message.channel.send(avatarList);
+	if (!message.mentions.users.size) {
+	    return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
 	}
+
+	const avatarList = message.mentions.users.map(user => {
+	    return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
+	});
+		
+	message.channel.send(avatarList);
+    }
 };
