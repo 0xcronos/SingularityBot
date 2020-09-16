fa2292ede4f1af05829a01a70697a85743c88edc
0xCronos <56097041+0xCronos@users.noreply.github.com>
2020-09-16 17:38:16 -0300

    Update getroles.js

--- commands/getroles.js
+++ commands/getroles.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'getroles',
-	description: 'Despliega lista con todos los roles del servidor',
+    name: 'getroles',
+    description: 'Despliega lista con todos los roles del servidor',
     aliases: ['gr'],
     args: false,
     guildOnly: true,
     rolesAllowed: ['Rey', 'Lord'],
-	execute(message) {
+    execute(message) {
         const roles = message.guild.roles.cache.map(role => {
             if(role.name !== "@everyone"){
                 return `> Rol: ${role.name}\n> Id: ||${role.id}||\n`
